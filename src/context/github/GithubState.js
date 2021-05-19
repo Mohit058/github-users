9dcac4c65f1dfeff06eab92bf15b7b1cb4366694
Mohit058 <singh.mohit058@gmail.com>
2021-05-19 10:15:46 +0530

    first commit

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,81 @@
+import {useReducer} from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+  SEARCH_USERS,
+  GET_USER,
+  GET_USER_REPOS,
+  CLEAR_USERS,
+  SET_LOADING
+} from '../types';
+
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production'){
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
+
+const GithubState = (props) => {
+  const initialState= {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  }
+
+ const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+ //SEARCH GITHUB USERS
+ const searchUsers = async (text) => {
+  setLoading();
+  const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+
+  dispatch({type: SEARCH_USERS, payload: res.data.items})
+}
+
+ //GET A SINGLE USER
+ const getUser = async (username) => {
+  setLoading();
+  const res = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+  
+  dispatch({type: GET_USER, payload: res.data})
+}
+
+ //GET USER REPOS
+ const getUserRepos = async (username) => {
+  setLoading();
+  const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+  
+  dispatch({type: GET_USER_REPOS, payload: res.data})
+}
+
+ //CLEAR USERS
+ const clearUsers = () => dispatch({type: CLEAR_USERS});
+
+ //SET LOADING
+ const setLoading = () => dispatch({type: SET_LOADING});
+
+
+ return <GithubContext.Provider value={{
+   users: state.users,
+   user: state.user,
+   repos: state.repos,
+   loading: state.loading,
+   searchUsers,
+   setLoading,
+   clearUsers,
+   getUser,
+   getUserRepos
+ }}>
+   {props.children}
+ </GithubContext.Provider>
+}
+
+export default GithubState;
